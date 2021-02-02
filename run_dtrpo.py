3276e03ff7ae8069fa14a39745150878acef3162
pierresdr <robustepierre@users.noreply.github.com>
2021-02-02 16:02:06 +0100

    print info during training

--- run_dtrpo.py
+++ run_dtrpo.py
@@ -21,10 +21,17 @@ def launch_dtrpo(args, seed):
     # Add stochasticity wrapper
     if args.force_stoch_env:
         env = StochActionWrapper(env, distrib=args.stoch_mdp_distrib, param=args.stoch_mdp_param)
+    update_message = 'Created env with observation space {} and action space {}'.format(
+            env.observation_space,  env.action_space)
+    prYellow(update_message)
 
     # Add the delay wrapper
     env = DelayWrapper(env, delay=args.delay, stochastic_delays=args.stochastic_delays, p_delay=args.delay_proba,
                        max_delay=args.max_delay)
+    update_message = 'Running env {} with initial dealy {}, stochastic delay is {},\
+             delay probability {}'.format(env, args.delay, args.stochastic_delays, args.delay_proba)
+    prYellow(update_message)
+
 
     # In the case of stochastic delays, the initial delay must be at least 1
     if args.stochastic_delays:
